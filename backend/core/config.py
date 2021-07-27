615f5d80da4be967a2b4a7f0eac05a502114a66b
Jaime <18090067@alumno.utmetropolitana.edu.mx>
2021-07-27 00:23:20 -0500

    second commit jobs database

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
+
+
+
 class Settings:
     PROJECT_TITLE: str ="fastjobs"
     PROJECT_VERSION: str ="0.1.1"
+
+    POTSGRES_USER:str = os.getenv("POSTGRES_USER")
+    POTSGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POTSGRES_SERVER: str = os.getenv("POSTGRES_SERVER","localhost")
+    POTSGRES_PORT: str = os.getenv("POSTGRES_PORT",5432)
+    POTSGRES_DB: str = os.getenv("POSTGRES_DB","db_jobs")
+    DATABASE_URL = f"postgresql://{POTSGRES_USER}:{POTSGRES_PASSWORD}@{POTSGRES_SERVER}:{POTSGRES_PORT}/{POTSGRES_DB}"
 
 settings = Settings()
